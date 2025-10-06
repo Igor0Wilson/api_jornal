224174d8c0f917ed3776ea2b9a6595054fe153d7
Igor Wilson <igorwilsonsimiao@gmail.com>
2025-10-06 19:09:06 -0300

    ajuste de env

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -3,12 +3,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const db = createPool({
-  host: process.env.DB_HOST, // interchange.proxy.rlwy.net
-  port: Number(process.env.DB_PORT), // 27807
+  host: process.env.DB_HOST, // api_jornal.railway.internal
   user: process.env.DB_USER, // root
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-  waitForConnections: true,
-  connectionLimit: 10,
-  queueLimit: 0,
+  password: process.env.DB_PASSWORD, // senha
+  database: process.env.DB_NAME, // railway
+  port: Number(process.env.DB_PORT), // 3306
 });
