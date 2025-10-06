97c2197cf8118f17b70ee6a72624d42a82857599
Igor Wilson <igorwilsonsimiao@gmail.com>
2025-10-06 19:26:44 -0300

    ajuste de login controller 2

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,11 +1,9 @@
 import { createPool } from "mysql2/promise";
-import dotenv from "dotenv";
-dotenv.config();
 
 export const db = createPool({
-  host: process.env.DB_HOST, // api_jornal.railway.internal
-  user: process.env.DB_USER, // root
-  password: process.env.DB_PASSWORD, // senha
-  database: process.env.DB_NAME, // railway
-  port: Number(process.env.DB_PORT), // 3306
+  host: "interchange.proxy.rlwy.net", // host privado do banco na Railway
+  user: "root", // usuário do banco
+  password: "HTvpKTvVhreWsPWYOTlmqLNoTvbXydZv", // senha do banco
+  database: "railway", // nome do banco
+  port: 27807, // porta do banco
 });
