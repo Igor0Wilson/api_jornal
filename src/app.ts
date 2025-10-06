0ebb90235c0d61cf95fd75b572069ea76b4cdd70
Igor Wilson <igorwilsonsimiao@gmail.com>
2025-10-06 17:52:56 -0300

    mudança no cors 3

--- src/app.ts
+++ src/app.ts
@@ -14,16 +14,19 @@ dotenv.config();
 
 const app = express();
 
-app.use(
-  cors({
-    origin: true, // qualquer frontend pode acessar
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
-    credentials: true, // permite cookies ou Authorization headers
-  })
-);
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  next();
+});
 
-app.options("*", cors());
+app.options("*", (req, res) => {
+  res.sendStatus(200);
+});
 
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
